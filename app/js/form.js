2ab0c8d9f885fcd3f7ab89cb54cc833327d6cca0
Mohannad <mohannadmostafa@outlook.com>
2017-02-05 07:41:46 -0500

    printing the log in the form page

--- app/js/form.js
+++ app/js/form.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         var rec = $("input.hash").val();
         var h_id = parseInt($("input.hid").val(), 10);
         var u_id = $("input.uid").val();
-        userContrat.uploadRecords()
+        userContrat.uploadRecords(rec, h_id, u_id);
+        addToLog("Input to BlockChain > " + rec + ", " + h_id + ", " + u_id);
     });
 });
